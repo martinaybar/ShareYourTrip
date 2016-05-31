bd56f0c7f1ca0f57919d63f0cb0d681e97629dd2
martinaybar <martinaybar@gmail.com>
2016-05-31 11:56:26 -0300

    Add project files.

---
+++ ShareYourTrip.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShareYourTrip.Web.MvcApplication" Language="C#" %>
---
+++ ShareYourTrip.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShareYourTrip.Web.Startup))]
+namespace ShareYourTrip.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
