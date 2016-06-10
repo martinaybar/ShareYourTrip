35c60c54d223098f62914ff68bacf3e7a6cdb1fd
martinaybar <martinaybar@gmail.com>
2016-06-10 18:01:25 -0300

    BusinessLayer and Services

---
+++ ShareYourTrip.Business/ShareYourTripService.cs
@@ -0,0 +1,27 @@
+﻿using ShareYourTrip.Data.IRepositories;
+using ShareYourTrip.Entities.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareYourTrip.Business
+{
+    public class ShareYourTripService
+    {
+
+        private readonly IUnitOfWork uow;
+
+        public ShareYourTripService(IUnitOfWork UnitOfWork)
+        {
+            this.uow = UnitOfWork;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return uow.Users.GetAll();
+        }
+
+    }
+}   
